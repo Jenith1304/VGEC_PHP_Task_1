f607fb29a2ec7ae68ad91b0cb54689a4bf687809
Jenith1304 <jenithpanchal1304@gmail.com>
2024-02-24 17:17:34 +0530

    Login Application Along With Captcha And Forget Password Mech

--- Login.php
+++ Login.php
@@ -1,6 +1,8 @@
 <?php
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 session_start();
 $con=mysqli_connect("localhost","root","","vgec_php_task_1");
+
 if(isset($_REQUEST["btn_login"]))
 {
     $logemail=$_REQUEST["txt_loginemail"];
@@ -16,7 +18,7 @@ if(isset($_REQUEST["btn_login"]))
         {
             $_SESSION["email"]=$row['Email Address'];
             $_SESSION["fname"]=$row["Fname"];
-            $_SESSION["login"]=true;
+            $_SESSION["login"]="true";
       ?>
      <script type="text/javascript"> alert("Login Successfull"); 
      window.location.href = "ToDo.php" </script>
@@ -41,6 +43,9 @@ if(isset($_REQUEST["btn_login"]))
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <!-- google recaptcha script -->
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
@@ -50,11 +55,8 @@ if(isset($_REQUEST["btn_login"]))
     <script src="script.js"></script>   
 </head>
 <body>  
-        <nav class="navbar bg-body-tertiary">
-             <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">To Do List Login</span>
-            </div>
-         </nav>
+    <!-- navbar included -->
+<?php include 'navbar.php'; ?>
     <!-- Login Form  -->
         <form method="post">
            <div class="container mt-5 border border-dark rounded" style="max-width:620px">
@@ -66,12 +68,14 @@ if(isset($_REQUEST["btn_login"]))
                <div class="form-group col-md-6 mt-2">
                      <label for="password">Password</label>
                      <input type="password" class="form-control" name="txt_loginpassword" required >
-                     <a href="RecoverPassword.php" class="btn btn-link">Forgot Your Password?</a>
+                     
                </div>
                <div class="g-recaptcha mt-2" data-sitekey="6Lf4NnwpAAAAACXhqvxWqh1VL6zjWDpGaJOCKSBd"></div>
                 <div class="form-group mt-3 mb-2 ">
                
                 <button type="submit" id="login" name="btn_login" class="btn btn-success">Login</button>
+                <a href="RecoverPassword.php" class="btn btn-link">Forgot Your Password?</a>
+                <a href="Registration.php" class="btn btn-link">Register</a>
                 </div>
 
                </div>
